a1b487fe75103c3ba391ce7ae880bd9318f22cda
rekko <rekkoi@gmail.com>
2017-08-15 22:29:30 +0900

    튜토리얼날 만든 테스트 클라이언트
    mac에서 동작

--- chat_client.py
+++ chat_client.py
@@ -1,9 +1,21 @@
 import socket
+import sys
+import select
+
+print('port? ')
+port = int(input())
 s = socket.socket()
-s.connect(('localhost', 10001))
+s.connect(('localhost', port))
 f = s.makefile('rw')
 while True:
-    print(input(), file=f)
-    f.flush()
-    print(f.readline())
-    
+    socket_list = [sys.stdin, s]
+
+    read_sockets, write_sockets, error_sockets = select.select(socket_list,
+                                                               [],
+                                                               [])
+
+    for sock in read_sockets:
+        if sock == s:
+            print(f.readline())
+        else:
+            print(input(), file=f, flush=True)
