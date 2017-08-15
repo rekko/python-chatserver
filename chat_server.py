05e01ee9f8086100bdc234c6c1bce964c8867dc1
rekko <rekkoi@gmail.com>
2017-08-15 22:28:06 +0900

    튜토리얼날 만든거

--- chat_server.py
+++ chat_server.py
@@ -1,20 +1,62 @@
 import socket
+import random
 from gevent.server import StreamServer
 
-users = []
+port = 10000 + random.randrange(40000)
+print('port ' + str(port))
 
+users = []
+nick = {}
+
+def get_nick(ca):
+    if ca in nick:
+        return nick[ca]
+    else:
+        nick[ca] = 'guest' + str(ca[1])      # default nick (port)
+        return nick[ca]
+
+def chat(line, sender_ca):
+    message = ""
+    sender = get_nick(sender_ca)
+    
+    print(str(sender_ca) + sender + "> " + line)
+    
+    if line.find('/') == 0 and len(line.split(' ')) > 1:
+        list = line.split(' ')
+        command = list[0]
+        arg = list[1]
+        if command == '/dice':
+            dice = random.randrange(int(arg))
+            message = sender + " roll dice : " + dice
+        elif command == '/nick':
+            nick[sender_ca] = arg
+            message = sender + " change nick -> " + nick[sender_ca]
+            message = process_command(line, sender)
+        else:
+            message = sender + "> " + line
+    else:
+        message = sender + "> " + line
+    
+    broadcast(message)
+
+def broadcast(message):
+    for u in users:
+        print(message, file=u, flush=True)
+        
 def handler(cs, ca):
     print(ca)
     f = cs.makefile('rw')
     users.append(f)
+    print('welcome', file=f, flush=True)
     try:
         while True:
-            line = f.readline()
-            for u in users:
-                print(line, file=u, flush=True)
+            line= f.readline().strip()
+            chat(line, ca)
+
     finally:
+        print('user ' + str(ca) + ' out')
         users.remove(f)
 
-server = StreamServer(('0.0.0.0', 10001),
+server = StreamServer(('0.0.0.0', port),
               handler)
 server.serve_forever()
